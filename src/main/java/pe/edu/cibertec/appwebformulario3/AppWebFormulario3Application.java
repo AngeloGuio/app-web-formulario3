b4993e3c4113aa428d9d2cdb9d026f1afd170b93
AngeloGuio <guioangelo.garcia@gmail.com>
2023-09-17 14:17:22 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appwebformulario3/AppWebFormulario3Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebformulario3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFormulario3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFormulario3Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebformulario3/AppWebFormulario3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebformulario3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFormulario3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
